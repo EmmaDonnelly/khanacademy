4a8701007b6dbd63b9a317d1e5007f73429e56a6
Em Don <emma.k.donnelly@gmail.com>
2017-02-23 21:57:31 +0000

    Create 001.making-drawings.js

---
+++ hour-of-code/drawing-with-code/001.making-drawings.js
@@ -0,0 +1,11 @@
+ellipse(212, 206, 283, 318);
+//face!
+//the first two numbers determine the position, with x and y, the second two control size
+//we call these paramaters to the elipse command (aka function). 
+
+ellipse(212, 250, 100, 73);
+//mouth
+
+ellipse(150, 150, 30, 30);
+ellipse(250, 250, 30, 30);
+//eyes
