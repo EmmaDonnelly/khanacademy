58641b47de6f2524227a98ca5999554cf2c8a678
Em Don <emma.k.donnelly@gmail.com>
2017-02-23 22:06:44 +0000

    Create 003.drawing-more-shapes.js

---
+++ hour-of-code/drawing-with-code/003.drawing-more-shapes.js
@@ -0,0 +1,22 @@
+ellipse(200, 200, 250, 300);  //face
+ellipse(200, 250, 100, 60); //mouth
+ellipse(150, 150, 30, 30);  //left eye
+ellipse(250, 150, 30, 30);  //right eye
+
+//neck
+rect(175, 345, 50, 50);
+//the parameters are x, y, width, height
+
+//the x and y parameters of the rect function control the top left corner
+//the x and y parameters of the ellipse function control the centre point
+
+//This would draw a robit face!
+//rect(76, 45, 250, 300); //face
+//rect(126, 250, 152, 60);  //mouth
+//rect(140, 150, 30, 30); //left eye
+//rect(240, 150, 30, 30); //right eye
+//rect(175, 345, 50, 50); //neck
+line(140, 129, 270, 129); //unibrow
+
+//there are so many similar looking rectangles, so comments help us keep track
+//the order matters, the program runs top to bottom and your shapes are opaque
