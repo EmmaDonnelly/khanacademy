1984fda21a5912c5f619abeb244467b24b13bb25
Em Don <emma.k.donnelly@gmail.com>
2017-02-28 22:54:58 +0000

    Create 006.sunny-snowy-day.js

---
+++ hour-of-code/drawing-with-code/006.sunny-snowy-day.js
@@ -0,0 +1,16 @@
+// The sky
+background(80, 196, 217);
+
+// The ground
+fill(104, 242, 109);
+rect(0, 300, 400, 100);  
+
+// The sun
+fill(250, 250, 3);
+ellipse(80, 64, 100, 100);  
+
+// The snowman
+fill(255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
