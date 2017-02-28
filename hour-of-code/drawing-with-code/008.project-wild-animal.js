51a6a1040f4d3ad977859aee3cb4d117c4ea96af
Em Don <emma.k.donnelly@gmail.com>
2017-02-28 23:19:46 +0000

    Create 008.project-wild-animal.js

---
+++ hour-of-code/drawing-with-code/008.project-wild-animal.js
@@ -0,0 +1,34 @@
+//sky
+background(201, 201, 201);
+//igloo
+noStroke();
+ellipse(1, 223, 290, 295);
+fill(133, 133, 133);
+ellipse(24, 195, 90, 90);
+
+//snow
+fill(255, 255, 255);
+rect(0, 200, 400, 200);
+
+//penguin 
+//wings
+fill(79, 40, 168);
+ellipse(215, 198, 123, 140);
+//body
+fill(172, 145, 230);
+ellipse(215, 198, 111, 139);
+ellipse(214, 124, 74, 68);
+//white bits
+fill(255, 255, 255);
+ellipse(200, 124, 30, 30);
+ellipse(228, 124, 30, 30);
+ellipse(215, 198, 74, 148);
+//face
+fill(0, 0, 0);
+ellipse(200, 124, 8, 8);
+ellipse(228, 124, 8, 8);
+fill(255, 119, 0);
+triangle(203, 137, 226, 137, 215, 155);
+//feet
+triangle(230, 274, 218, 252, 264, 265);
+triangle(204, 274, 214, 252, 171, 265);
