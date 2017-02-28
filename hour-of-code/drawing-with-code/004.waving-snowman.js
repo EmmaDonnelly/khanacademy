93061cae996b636a2b4138773e350a9c446f9de9
Em Don <emma.k.donnelly@gmail.com>
2017-02-28 22:43:22 +0000

    Update 004.waving-snowman.js

--- hour-of-code/drawing-with-code/004.waving-snowman.js
+++ hour-of-code/drawing-with-code/004.waving-snowman.js
@@ -1,6 +1,11 @@
 //ground
-rect(0,380,400,20);
+rect(0, 360, 398, 39);
 //snowman body
-ellipse(200,300,150,150);
-ellipse(200,200,100,100;
-ellipse(200,120,75,75);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+//snowman head
+ellipse(200, 120, 75, 75);
+//left arm
+line(160, 171, 100, 100);
+//right arm
+line(240, 171, 300, 100);
