daf2169730c7d35f93472d826126e37f4cf1b498
Em Don <emma.k.donnelly@gmail.com>
2017-03-05 19:51:37 +0000

    Update 007.project-super-snowman.js

--- hour-of-code/drawing-with-code/007.project-super-snowman.js
+++ hour-of-code/drawing-with-code/007.project-super-snowman.js
@@ -1,4 +1,43 @@
-// simple snowman
+//sky
+background(201, 201, 201);
+//snow
+noStroke();
+fill(255, 255, 255);
+rect(0, 200, 400, 200);
+ellipse(10, 230, 320, 146);
+ellipse(390, 211, 302, 70);
+//snowman body
+stroke(0, 0, 0);
 ellipse(200, 300, 150, 150);
+fill(255, 255, 0);
+ellipse(216, 187, 100, 82);
+fill(255, 255, 255);
 ellipse(200, 200, 100, 100);
+//snowman scarf
+fill(255, 255, 0);
+ellipse(200, 151, 75, 62);
+//snowman hat
+fill(255, 0, 0);
+triangle(135, 43, 221, 73, 203, 157);
+ellipse(197, 96, 75, 62);
+fill(255, 255, 255);
+ellipse(143, 49, 25, 25);
+//snowman head
+fill(255, 255, 255);
 ellipse(200, 120, 75, 75);
+// snowman face
+fill(0, 0, 0);
+ellipse(183, 120, 10, 10);
+ellipse(217, 120, 10, 10);
+fill(255, 119, 0);
+triangle(192, 129, 210, 128, 203, 157);
+//snowman buttons
+fill(0, 0, 0);
+ellipse(200, 196, 10, 10);
+ellipse(200, 215, 10, 10);
+ellipse(200, 235, 10, 10);
+//left arm
+strokeWeight(4); 
+line(160, 190, 80, 100);
+//right arm
+line(240, 190, 310, 100);
